1145a1ba041c3de666423e8fcf28f111653f22d5
JIgorSilva <jigorsilva@hotmail.com>
2025-03-21 10:24:25 -0300

    feat: implementação controller rest

--- springbootrestlearning/src/main/java/br/com/learning/springbootrestlearning/controller/GreetingController.java
+++ springbootrestlearning/src/main/java/br/com/learning/springbootrestlearning/controller/GreetingController.java
@@ -1,4 +1,20 @@
 package br.com.learning.springbootrestlearning.controller;
 
+import br.com.learning.springbootrestlearning.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class GreetingController {
+
+    private static final String template = "HELLO WORLD";
+    private final AtomicLong countertId = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name",defaultValue = "World")String name){
+        return new Greeting(countertId.incrementAndGet(), String.format(template,name));
+    }
 }
